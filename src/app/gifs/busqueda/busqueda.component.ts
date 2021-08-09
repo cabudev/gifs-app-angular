29c139579fd18354283e6e7707994663efed22d7
cabudev <budaimc@gmail.com>
2021-08-09 23:47:10 +0200

    first commit

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+
+export class BusquedaComponent implements OnInit {
+  
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  constructor( private gifsService: GifsService){
+
+  }
+
+  //buscar(termino: string) {
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    this.gifsService.buscarGifs( valor );
+    this.txtBuscar.nativeElement.value='';
+  }
+  
+  ngOnInit(): void {
+  }
+
+}
